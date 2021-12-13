60464608a3778897cdd686067bd319c92771161d
trinh van dat <trinhvandat90399@gmail.com>
2021-12-14 05:06:57 +0700

    create Project entity

---
+++ agile-pm-tool/src/main/java/org/aibles/agilepmtool/domain/Project.java
@@ -0,0 +1,119 @@
+package org.aibles.agilepmtool.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Table(name = "project")
+@Entity
+public class Project implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "project_name")
+    private String projectName;
+
+    @Column(name = "project_identifier")
+    private String projectIdentifier;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date endDate;
+
+    @Column(name = "created_at")
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updatedAt;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
