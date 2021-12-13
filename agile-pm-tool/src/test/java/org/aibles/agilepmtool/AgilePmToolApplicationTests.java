0163a5183437c20469cc8b5971283ecd582ec6ba
trinh van dat <trinhvandat90399@gmail.com>
2021-12-14 04:58:01 +0700

    Create project, add file changelog for project table

---
+++ agile-pm-tool/src/test/java/org/aibles/agilepmtool/AgilePmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.agilepmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilePmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
