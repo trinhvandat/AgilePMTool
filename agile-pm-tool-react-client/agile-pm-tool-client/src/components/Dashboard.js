884ed8efb0eeae0659d2e2de4d8de89978b64908
trinh van dat <trinhvandat90399@gmail.com>
2021-12-16 11:11:04 +0700

    create projectItem component, Header for Main app

--- agile-pm-tool-react-client/agile-pm-tool-client/src/components/Dashboard.js
+++ agile-pm-tool-react-client/agile-pm-tool-client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
 
-export default class Dashboard extends Component {
+export class Dashboard extends Component {
     render() {
         return (
             <div>
                 <h1>Hello! My name is Leonard.</h1>
+                <ProjectItem />
             </div>
-        )
+        );
     }
 }
+
+export default Dashboard;
